f777220216ccf59fa55b2f54550e46fb08c2fddb
dev_ans <developer.ansouza@gmail.com>
2022-07-20 01:40:08 -0300

    Update clock date and time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
+//Definition of day
+const dateToday = new Date();
+dateToday.toLocaleDateString("en-US", {timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone});
+
+//Definition of constants
+const body_clock = document.getElementById('body_clock');
+const today = document.getElementById("today");
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
-const body_clock = document.getElementById('body_clock');
-const dateToday = new Date();
+
+//Definition of constants to week day and months of year
+const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+//Change text of date
+today.textContent = weekday[dateToday.getDay()] + ', ' + dateToday.getDate() + ' ' + months[dateToday.getMonth()] + ' ' + dateToday.getFullYear();
+
+//Change clock time
 const clock = setInterval(function time () {
     const dateToday = new Date();
     dateToday.toLocaleDateString("en-US", {timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone});
-    
+
     let hours_right_now = dateToday.getHours();
     let minutes_right_now = dateToday.getMinutes();
     let seconds_right_now = dateToday.getSeconds();
@@ -16,6 +30,7 @@ const clock = setInterval(function time () {
     if(seconds_right_now < 10) seconds.textContent = "0" + seconds_right_now; else seconds.textContent = seconds_right_now;
 })
 
+//Change backgroud-image denpending of hours
 if(dateToday.getHours() > 18 || dateToday.getHours() < 6){
     body_clock.style["backgroundImage"] = "url(\"/clock/assets/images/night.jpg\")";
 }
